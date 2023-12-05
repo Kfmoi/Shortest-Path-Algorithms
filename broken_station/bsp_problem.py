53b07e61a1770d9b23066567814ca15f8d7a8ac7
Kfmoi <faragk1@mcmaster.ca>
2023-12-05 12:05:26 -0500

    Another solution to the problem

--- broken_station/bsp_problem.py
+++ broken_station/bsp_problem.py
@@ -1,79 +1,73 @@
-# Helper function to count the removed stations based on the given distance using dynamic programming
-def count_removed_dp(start, distance_left, length, L, memo):
-    # Check if the result for the current parameters is already in the memo
-    if (start, distance_left) in memo:
-        return memo[(start, distance_left)]
+def calculate_max(mem, L, m):
+    max_value = 0
 
-    # Base case: if the start index is beyond the number of stations or if the distance left is zero
-    if start >= length or distance_left == 0:
+    # Base case: if list is empty return 0
+    if len(L) == 0:
         return 0
 
-    count = 0
+    # Base case: if there are no more elements to be removed then,
+    # return the minimum difference of the adjacent elements
+    if m == 0:
+        return min(L[i + 1] - L[i] for i in range(len(L) - 1))
 
-    # Loop through the remove possible stations
-    for i in range(start + 1, length):
-        # Recursively count removed stations for the remaining stations and distance_left
-        if L[i] - L[start] < distance_left:
-            count += 1 + count_removed_dp(i, distance_left - (L[i] - L[start]), length, L, memo)
+    # Loop through each element of the list
+    for i in range(len(L)):
+        # Create a list with the current element removed
+        mod_L = L[:i] + L[i + 1:]
 
-    # Memorize the count
-    memo[(start, distance_left)] = count
-    return count
+        # Check if the value is located in memory or not
+        # If it is not located then compute it and store the result
+        if (tuple(mod_L), m - 1) not in mem:
+            mem[tuple(mod_L), m - 1] = calculate_max(mem, mod_L, m - 1)
 
+        # Retrieve the result from memory
+        mem_value = mem[tuple(mod_L), m - 1]
 
-# Function to return the maximum value possible of the closest two number in L, after removing m of the numbers
-def bsp_value_dp(L, m):
-    length = len(L)
+        # Update the maximum value
+        if mem_value > max_value:
+            max_value = mem_value
 
-    # Memorization dict to store computed results
-    memo = {}
-    low = 0
-    high = L[-1] - L[0]
-
-    # Binary search to find the maximum distance such that the number of removed stations doesn't exceed 'm
-    while low < high:
-        mid = (low + high + 1) // 2
-        removed = count_removed_dp(0, mid, length, L, memo)
-
-        # Adjust the parameters to remove the stations
-        if removed > m:
-            high = mid - 1
-        else:
-            low = mid
-
-    return low
+    return max_value
 
 
-# Function to return the corresponding list after removing m value
-def bsp_solution_dp(L, m):
-    result = []
+def find_solution(mem, result):
+    # Loop through the memory
+    for key, val in mem.items():
+        # Extract the list from the key
+        curr_L = list(key[0])
+        # Calculate the minimum difference of the adjacent elements
+        min_diff = min(curr_L[i + 1] - curr_L[i] for i in range(len(curr_L) - 1))
+        # If it is equal to the computed result then return the list as it meets the criteria
+        if min_diff == result:
+            return curr_L
 
-    # Calculate the max distance using the bsp_value_dp function
-    max_distance = bsp_value_dp(L, m)
+    # Case if there is no list that meets the criteria of the result
+    return []
 
-    length = len(L)
-    last = L[0]
-    result.append(last)
-    temp = m
 
-    # Construct the resultant list after station removal based on the maximum distance
-    for i in range(1, length):
-        if L[i] - last < max_distance and temp > 0:
-            # Skip station if the distance to the last station is less than the max distance and removal limit is not
-            # exhausted
-            temp -= 1
-        else:
-            # Add station to the result list if the distance is greater or removal limit is reached
-            result.append(L[i])
-            last = L[i]
+def bsp_value(L, m):
+    # Create the dict to store the results
+    mem = {}
+    return calculate_max(mem, L, m)
 
-    return result  # Return the resultant list after removing stations
+
+def bsp_solution(L, m):
+    # Create the dict to store the results
+    mem = {}
+
+    # Calculate the maximum value to be used to get the solution
+    result = calculate_max(mem, L, m)
+
+    # Find the solution from the memory dict
+    solution = find_solution(mem, result)
+
+    return solution
 
 
 if __name__ == '__main__':
     L = [2, 4, 6, 7, 10, 14]
-    m = 2
-    value = bsp_value_dp(L, m)
-    answer = bsp_solution_dp(L, m)
-    print(answer)
+    m = 4
+    value = bsp_value(L, m)
+    answer = bsp_solution(L,m)
     print(value)
+    print(answer)
