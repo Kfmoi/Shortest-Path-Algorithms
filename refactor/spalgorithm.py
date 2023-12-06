c8d67dce5149133aef4e110cc21d6b26b182abbb
Kfmoi <faragk1@mcmaster.ca>
2023-12-05 19:05:41 -0500

    Added adapter for the A star algorithm

--- refactor/spalgorithm.py
+++ refactor/spalgorithm.py
@@ -59,44 +59,44 @@ class Bellman_Ford(SPAlgorithm):
 
 class A_Star(SPAlgorithm):
     def calc_sp(self, graph: Graph, source: int, dest: int) -> float:
-        def a_star(G, s, d, h):
-            g_distances = {}
-            predecessors = {}
-            f_scores = {}
-            # Keep track of all the nodes in the priority queue. This is to make sure that nodes that were popped
-            # previously from the heap but have gotten better scores from the heuristics are reconsidered
-            in_pq: dict[int, bool] = {}
-            pq = MinHeap([])
-            for node in G.adj.keys():
-                g_distances[node] = float('inf')
-                f_scores[node] = float('inf')
-                pq.insert(Element(node, f_scores[node]))
-                in_pq[node] = True
+        dist = 
 
-            g_distances[s] = 0
-            f_scores[s] = g_distances[s] + h[s]
-            pq.decrease_key(s, f_scores[s])
-            while not pq.is_empty():
-                cur_elem = pq.extract_min()
-                cur_node, cur_score = cur_elem.value, cur_elem.key
-                in_pq[cur_node] = False
-                if cur_node == d:
-                    break
-                for nbr in G.adj[cur_node]:
-                    new_dist = g_distances[cur_node] + G.w(cur_node, nbr)
-                    if new_dist < g_distances[nbr]:
-                        predecessors[nbr] = cur_node
-                        g_distances[nbr] = new_dist
-                        f_scores[nbr] = new_dist + h[nbr]
-                        # If the neighbour is in the pq currently then decrease the key
-                        # Otherwise re-insert it (this is for the edge case that the heuristic might overshoot but
-                        # isn't incorrect)
-                        if in_pq[nbr]:
-                            pq.decrease_key(nbr, f_scores[nbr])
-                        else:
-                            pq.insert(Element(nbr, f_scores[nbr]))
+    def __a_star(graph: Graph, source: int, dest: int, heuristic: dict[int, float]) -> tuple[dict[int, int], int]:
+        g_distances = {}
+        predecessors = {}
+        f_scores = {}
+        # Keep track of all the nodes in the priority queue. This is to make sure that nodes that were popped
+        # previously from the heap but have gotten better scores from the heuristics are reconsidered
+        in_pq: dict[int, bool] = {}
+        pq = MinHeap([])
+        for node in graph.adj.keys():
+            g_distances[node] = float('inf')
+            f_scores[node] = float('inf')
+            pq.insert(Element(node, f_scores[node]))
+            in_pq[node] = True
 
-            return g_distances[d]
+        g_distances[source] = 0
+        f_scores[source] = g_distances[source] + heuristic[source]
+        pq.decrease_key(source, f_scores[source])
+        while not pq.is_empty():
+            cur_elem = pq.extract_min()
+            cur_node, cur_score = cur_elem.value, cur_elem.key
+            in_pq[cur_node] = False
+            if cur_node == dest:
+                break
+            for nbr in graph.adj[cur_node]:
+                new_dist = g_distances[cur_node] + graph.w(cur_node, nbr)
+                if new_dist < g_distances[nbr]:
+                    predecessors[nbr] = cur_node
+                    g_distances[nbr] = new_dist
+                    f_scores[nbr] = new_dist + heuristic[nbr]
+                    # If the neighbour is in the pq currently then decrease the key
+                    # Otherwise re-insert it (this is for the edge case that the heuristic might overshoot but
+                    # isn't incorrect)
+                    if in_pq[nbr]:
+                        pq.decrease_key(nbr, f_scores[nbr])
+                    else:
+                        pq.insert(Element(nbr, f_scores[nbr]))
 
-        heuristic = graph.get_heuristic()
-        return a_star(graph, source, dest, heuristic)
+        return predecessors, g_distances[dest]
+
